5085c0222e8176f8eeb7c04b2ed29e3f16239fac
Maciej Kłoda <maciej.kloda98@gmail.com>
2023-02-13 13:43:33 +0100

    Moved assets to src

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import Nav from './Components/Nav/Nav.js';
 import './App.css';
+import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
 import HeroesFeatured from './Components/HeroesFeatured/HeroesFeatured.js';
 
 function App() {
   return (
     <>
-    <Nav></Nav>
-    <main>
-      <div className='container'>
-        <HeroesFeatured />
-      </div>
-    </main> 
-    <footer></footer>
+    <Router>
+      <Nav />
+      <main>
+        <div className='container'>
+          <Routes >
+            <Route exact path='/' element={<HeroesFeatured />}>
+            </Route>
+            <Route path='/hero/:id'>
+            </Route>
+          </Routes >
+        </div>
+      </main> 
+      <footer>
+        <div className="container">
+          <p>This content is kindly provided by <a href="https://www.superheroapi.com/">Superhero API</a></p>
+        </div>
+      </footer>
+      </Router>
     </>
   );
 }
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import logo from '../../assets/img/nav_logo.png'
+import { Link } from "react-router-dom";
 import './Nav.css';
 
 function Nav() {
+    const [searchInputValue, setSearchInputValue] = useState('');
+
     return (
-        <nav></nav>
+        <nav className="nav">
+            <div className="container">
+                <Link to="/"><img className="nav__logo" src={logo} alt="Superhero Database" /></Link>
+                <div className="nav__search">
+                    <input onChange={event => {setSearchInputValue(event.target.value)}} value={searchInputValue} type="text" name="search"></input>
+                    <Link to={`/search/${searchInputValue}`}><button>find hero!</button></Link>
+                </div>
+            </div>
+        </nav>
     );
 
 }
