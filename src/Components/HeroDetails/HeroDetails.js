0ddd548d4661d78e1875bf6e4a21b822f6c09098
Maciej Kłoda <maciej.kloda98@gmail.com>
2023-02-24 13:33:01 +0100

    Made detailed hero view

--- src/Components/HeroDetails/HeroDetails.js
+++ src/Components/HeroDetails/HeroDetails.js
@@ -30,8 +30,25 @@ function HeroDetails() {
             { !isLoading && ( 
                 <div className='detailed__hero'>
                     <h2>{detailedHeroesList.name}</h2> 
-                    <img className='featu' src={detailedHeroesList.image.url} alt='Intelligence icon'/>
+                    <img className='detailed__hero__img' src={detailedHeroesList.image.url} alt='Intelligence icon'/>
                     <Powerstats powerstats={detailedHeroesList.powerstats}></Powerstats>
+                    <div className='detailed__hero__appearance'>
+                        <h2>Appearance</h2>
+                        {Object.keys(detailedHeroesList.appearance).map((key) => (<p>{key}: {detailedHeroesList.appearance[key]}</p>))}
+                    </div>
+                    <div className='detailed__hero__biography'>
+                        <h2>Biography</h2>
+                        {Object.keys(detailedHeroesList.biography).map((key) => (<p>{key}: {detailedHeroesList.biography[key]}</p>))}
+                    </div>
+                    <div className='detailed__hero__connections'>
+                        <h2>Connections</h2>
+                        {Object.keys(detailedHeroesList.connections).map((key) => (<p>{key}: {detailedHeroesList.connections[key]}</p>))}
+                    </div>
+                    <div className='detailed__hero__work'>
+                        <h2>Work</h2>
+                        {Object.keys(detailedHeroesList.work).map((key) => (<p>{key}: {detailedHeroesList.work[key]}</p>))}
+                    </div>
+                    {console.log(detailedHeroesList)}
                 </div>
             )}
             { isLoading && <div className="loader-container"><Loader /></div> }
--- src/Components/Powerstats/Powerstats.js
+++ src/Components/Powerstats/Powerstats.js
@@ -4,25 +4,25 @@ import './Powerstats.css';
 
 function Powerstats({ powerstats }) {
     return (
-        <div className='featured__hero__stats'>
+        <div className='hero__stats'>
             <div>
-                <img className='featured__hero__stats__icon' src={icon.boxingIcon} alt='Combat icon'/>
+                <img className='hero__stats__icon' src={icon.boxingIcon} alt='Combat icon'/>
                 <p>{powerstats.combat}</p>
             </div>
             <div>
-                <img className='featured__hero__stats__icon' src={icon.durableIcon} alt='Durability icon'/>
+                <img className='hero__stats__icon' src={icon.durableIcon} alt='Durability icon'/>
                 <p>{powerstats.durability}</p>
             </div>
             <div>
-                <img className='featured__hero__stats__icon' src={icon.thinkingIcon} alt='Intelligence icon'/>
+                <img className='hero__stats__icon' src={icon.thinkingIcon} alt='Intelligence icon'/>
                 <p>{powerstats.intelligence}</p>
             </div>
             <div>
-                <img className='featured__hero__stats__icon' src={icon.speedometerIcon} alt='Speed icon'/>
+                <img className='hero__stats__icon' src={icon.speedometerIcon} alt='Speed icon'/>
                 <p>{powerstats.speed}</p>
             </div>
             <div>
-                <img className='featured__hero__stats__icon' src={icon.fistIcon} alt='Strength icon'/>
+                <img className='hero__stats__icon' src={icon.fistIcon} alt='Strength icon'/>
                 <p>{powerstats.strength}</p>
             </div>
         </div>
