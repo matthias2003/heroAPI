f2e445cb5a516fc6dbefb1f0f006e3efa60f9f32
Maciej Kłoda <maciej.kloda98@gmail.com>
2023-02-27 21:12:36 +0100

    Rework powerstats

--- src/Components/HeroDetails/HeroDetails.js
+++ src/Components/HeroDetails/HeroDetails.js
@@ -2,59 +2,68 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router";
 import Loader from "../Loader/Loader.js";
 import { getTotalHeroInfoById } from '../../requests.js';
+import * as icon from '../../assets/icons/index.js'
 import './HeroDetails.css';
-import Powerstats from '../Powerstats/Powerstats.js';
+
 
     
 function HeroDetails() {
     const { id } = useParams();
     const [ isLoading, setLoadingState] = useState(true);
-    const [detailedHeroesList, setDetailedHeroesList] = useState({});
+    const [ detailedHero, setDetailedHeroes ] = useState({});
 
     useEffect(() => {
         setLoadingState(true);
-        getTotalHeroInfoById(id).then(searchResults => {
-            const { data } = searchResults;
-
-            if (data.error) {
-                return;
-            }
-
-            setDetailedHeroesList(data);
-            setLoadingState(false);
-        });
+        fetchDetailedHero();
     },[id])
+
+    const fetchDetailedHero = async () => {
+        const { data } = await getTotalHeroInfoById(id);
+        setDetailedHeroes(data);
+        setLoadingState(false);
+    }
 
     return (
         <section className='detailed'>
             { !isLoading && ( 
                 <div className='detailed__hero'>
-                    <h2>{detailedHeroesList.name}</h2> 
-                    <img className='detailed__hero__img' src={detailedHeroesList.image.url} alt='Intelligence icon'/>
-                    <Powerstats powerstats={detailedHeroesList.powerstats}></Powerstats>
+                    <h2>{detailedHero.name}</h2> 
+                    <img className='detailed__hero__img' src={detailedHero.image.url} alt='Intelligence icon'/>
+                    <div className='detailed__hero__stats'> 
+                        {
+                          Object.entries(detailedHero.powerstats).filter(([tab]) => { return ['combat','durability','intelligence','power','speed','strength'].includes(tab)}).map(([key, value]) => {
+                              return(
+                                  <div key={id+key}>
+                                      <img className='detailed__hero__stats__icon' src={icon[key]} alt={key} />
+                                      <p>{value}</p>
+                                      <p>{key}</p>
+                                  </div>
+                          )})
+                        }
+                    </div>
                     <div className='detailed__hero__appearance'>
                         <h2>Appearance</h2>
-                        {Object.keys(detailedHeroesList.appearance).map((key) => (<p>{key}: {detailedHeroesList.appearance[key]}</p>))}
+                        {Object.keys(detailedHero.appearance).map((key) => (<p>{key}: {detailedHero.appearance[key]}</p>))}
                     </div>
+
                     <div className='detailed__hero__biography'>
                         <h2>Biography</h2>
-                        {Object.keys(detailedHeroesList.biography).map((key) => (<p>{key}: {detailedHeroesList.biography[key]}</p>))}
+
+                        {Object.keys(detailedHero.biography).map((key) => (<p>{key}: {detailedHero.biography[key]}</p>))}
                     </div>
                     <div className='detailed__hero__connections'>
                         <h2>Connections</h2>
-                        {Object.keys(detailedHeroesList.connections).map((key) => (<p>{key}: {detailedHeroesList.connections[key]}</p>))}
+                        {Object.keys(detailedHero.connections).map((key) => (<p>{key}: {detailedHero.connections[key]}</p>))}
                     </div>
                     <div className='detailed__hero__work'>
                         <h2>Work</h2>
-                        {Object.keys(detailedHeroesList.work).map((key) => (<p>{key}: {detailedHeroesList.work[key]}</p>))}
+                        {Object.keys(detailedHero.work).map((key) => (<p>{key}: {detailedHero.work[key]}</p>))}
                     </div>
-                    {console.log(detailedHeroesList)}
                 </div>
             )}
             { isLoading && <div className="loader-container"><Loader /></div> }
         </section>
     );
-
 }
 
 export default HeroDetails;
