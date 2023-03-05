41bf0ae02f2127b3bda6160866796d2b81d2e912
Maciej Kłoda <maciej.kloda98@gmail.com>
2023-03-05 21:10:32 +0100

    Small css fixes

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -12,13 +12,12 @@ function Nav() {
             <div className="container">
                 <Link to="/"><img className="nav__logo" src={logo} alt="Superhero Database" /></Link>
                 <div className="nav__search">
-                    <input onChange={event => {setSearchInputValue(event.target.value)}} value={searchInputValue} type="text" name="search"></input>
+                    <input onChange={event => {setSearchInputValue(event.target.value)}} value={searchInputValue} type="text" name="search" placeholder="Search hero"></input>
                     <Link to={`/search/${searchInputValue}`}><button><img src={search} alt="Search icon" /></button></Link>
                 </div> 
             </div>
         </nav>
     );
-
 }
 
 export default Nav;
