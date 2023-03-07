8e491b4cdb627f1dddaf8aeaef2acbd183397c91
Maciej Kłoda <maciej.kloda98@gmail.com>
2023-03-07 20:54:24 +0100

    Styled ErrorInfo, added image to error

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -12,7 +12,8 @@ function Nav() {
             <div className="container">
                 <Link to="/"><img className="nav__logo" src={logo} alt="Superhero Database" /></Link>
                 <div className="nav__search">
-                    <input onChange={event => {setSearchInputValue(event.target.value)}} value={searchInputValue} type="text" name="search" placeholder="Search hero"></input>
+                    <input onChange={event => {setSearchInputValue(event.target.value)}} value={searchInputValue} type="text" name="search" placeholder="Search hero"
+                           onKeyDown={ event => { if (event.key === "Enter") setSearchInputValue(event.target.value) } /* TODO: trzeba zrobić, żeby zatwierdzać enterem*/} ></input>
                     <Link to={`/search/${searchInputValue}`}><button><img src={search} alt="Search icon" /></button></Link>
                 </div> 
             </div>
