be428ac8afb129ca7359844bd9a7c7425be0c3b8
Maciej Kłoda <maciej.kloda98@gmail.com>
2023-03-09 18:29:37 +0100

    Handled search on enter, changed nav view in smaller devices

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,19 +1,25 @@
 import React, { useState } from "react";
 import logo from '../../assets/img/nav_logo.png'
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import search from "../../assets/icons/search.png";
 import './Nav.css';
 
 function Nav() {
     const [ searchInputValue, setSearchInputValue ] = useState('');
+    const navigate = useNavigate();
+
+    const searchRedirect = (event) => {
+        setSearchInputValue(event.target.value);
+        navigate(`/search/${searchInputValue}`);
+    }
 
     return (
         <nav className="nav">
             <div className="container">
                 <Link to="/"><img className="nav__logo" src={logo} alt="Superhero Database" /></Link>
                 <div className="nav__search">
-                    <input onChange={event => {setSearchInputValue(event.target.value)}} value={searchInputValue} type="text" name="search" placeholder="Search hero"
-                           onKeyDown={ event => { if (event.key === "Enter") setSearchInputValue(event.target.value) } /* TODO: trzeba zrobić, żeby zatwierdzać enterem*/} ></input>
+                    <input onChange={event => { setSearchInputValue(event.target.value)}} value={searchInputValue} type="text" name="search" placeholder="Search hero"
+                           onKeyDown={event => { if (event.key === "Enter") {searchRedirect(event)}}}></input>    {/* TODO: trzeba zrobić, żeby zatwierdzać enterem*/}
                     <Link to={`/search/${searchInputValue}`}><button><img src={search} alt="Search icon" /></button></Link>
                 </div> 
             </div>
