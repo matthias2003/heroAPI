3c4bea6d51770e4f1bc76579600648baec128b7a
matthias03 <maciej.kloda98@gmail.com>
2023-10-30 13:28:11 +0100

    App prepared to web hosting

--- src/requests.js
+++ src/requests.js
@@ -1,20 +1,15 @@
 import axios from 'axios';
 
-
 export const getBasicHeroInfoById = async (id) => {
-    // const { data } = await axios.get(`https://hero-app-server-proxy.vercel.app`);
-    const { data } = await axios.get(`http://127.0.0.1:3001`);
+    const { data } = await axios.get(`https://hero-app-server-proxy.vercel.app`);
     return data;
 }
 
 export const searchHeroesByName = async (name) => {
-    return axios.get(`http://127.0.0.1:3001/search/${name}`);
-    // const { data } = await axios.get(`https://hero-app-server-proxy.vercel.app/search/${name}`);
-    //return axios.get(`https://superheroapi.com/api/${ACCESS_TOKEN}/search/${name}`);
+    return axios.get(`https://hero-app-server-proxy.vercel.app/search/${name}`);
 }
 
 export const getTotalHeroInfoById = async (id) => {
-    const { data } = await axios.get(`http://127.0.0.1:3001/details/${id}`);
-    //const { data } = await axios.get(`https://hero-app-server-proxy.vercel.app/details/${name}`)
+    const { data } = await axios.get(`https://hero-app-server-proxy.vercel.app/details/${name}`)
     return data;
 }
