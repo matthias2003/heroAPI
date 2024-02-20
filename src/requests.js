2efec49c5729e20fa74a88a50da384289dbe2444
matthias03 <maciej.kloda98@gmail.com>
2024-02-20 21:33:36 +0100

    Changed the domain of requests

--- src/requests.js
+++ src/requests.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
 export const getBasicHeroInfoById = async (id) => {
-    const { data } = await axios.get(`https://hero-app-server-proxy.vercel.app`);
+    const { data } = await axios.get(`https://hero-app-server-proxy.maciejkloda.pl`);
     return data;
 }
 
 export const searchHeroesByName = async (name) => {
-    return axios.get(`https://hero-app-server-proxy.vercel.app/search/${name}`);
+    return axios.get(`https://hero-app-server-proxy.maciejkloda.pl/search/${name}`);
 }
 
 export const getTotalHeroInfoById = async (id) => {
-    const { data } = await axios.get(`https://hero-app-server-proxy.vercel.app/details/${id}`)
+    const { data } = await axios.get(`https://hero-app-server-proxy.maciejkloda.pl/details/${id}`)
     return data;
 }
